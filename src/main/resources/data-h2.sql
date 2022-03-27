1e82bd87c4e797dfea157bb1832bf2080e44e703
Dries Steenberghe <3s@steenberghe.com>
2022-03-27 14:26:28 +0200

    GACP

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE IF NOT EXISTS words (
+    word varchar(16) PRIMARY KEY,
+    length smallint
+); 
+
+
+-- Extracted from: https://github.com/OpenTaal/opentaal-wordlist/
+INSERT INTO words (word, length) VALUES 
+	('aagje', 5),
+	('aaibaar', 7),
+	('aaien', 5),
+	('aaiing', 6),
+	('aalbes', 6),
+	('aalfuik', 7),
+	('aalgeer', 7),
+	('aalglad', 7),
+	('aalkorf', 7),
+	('aalkuip', 7);
